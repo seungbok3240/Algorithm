c85def512c62de7448652dac501f3a4f0aa3e3a3
seungbok3240 <seungbok3240@gmail.com>
2022-02-13 22:00:21 +0900

    using two deque

--- 1966.py
+++ 1966.py
@@ -16,3 +16,25 @@ for _ in range(t):
                 break
         else:
             paper_list.rotate(-1)
+
+# from collections import deque
+#
+# t = int(input())
+# for _ in range(t):
+#     n, m = map(int, input().split())
+#     importance = deque(list(map(int, input().split())))
+#     index = deque([i for i in range(n)])
+#     target_importance = importance[m]
+#     target_index = index[m]
+#     cnt = 0
+#     while True:
+#         if importance[0] == max(importance):
+#             tmp_importance = importance.popleft()
+#             tmp_index = index.popleft()
+#             cnt += 1
+#             if target_importance == tmp_importance and target_index == tmp_index:
+#                 print(cnt)
+#                 break
+#         else:
+#             importance.rotate(-1)
+#             index.rotate(-1)
